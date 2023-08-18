8596fb193397e27f0d2e91dcb5f1ec903377c3bc
Andy <Andyjin@lilith.com>
2023-08-18 23:55:12 +0800

    fix: bug

--- api/main-vercel.ts
+++ api/main-vercel.ts
@@ -2,20 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from '../src/app.module';
 import express from 'express';
-import cors from 'cors'; // <-- 导入cors
+import cors from 'cors';
 
 const server = express();
-
-// 启用CORS
 server.use(cors());
 
+let app;
+
 const startNestApplication = async (expressInstance) => {
-  const app = await NestFactory.create(
+  const nestApp = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
   );
-
-  return app.init();
+  await nestApp.init();
 };
 
 startNestApplication(server);
+
+export default (req, res) => {
+  if (!app) {
+    startNestApplication(server)
+      .then(() => server(req, res))
+      .catch((err) => console.error(err));
+  } else {
+    server(req, res);
+  }
+};
